d5d22a7f681a9cfd37b6d32cd6a2ac79f47e6d5d
[Xime Mzo] <54044787+ximenamzo@users.noreply.github.com>
2023-03-30 08:55:42 -0600

    Avance marzo 30
    Solo avances en disenio

--- SmartCrowd/settings.py
+++ SmartCrowd/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=t(74_hwezje(^xy2kmzb=lddqpqtz*e9x$ov-u-0x%t_7*p!7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
-ALLOWED_HOSTS = ['smartcrowd.azurewebsites.net']
+#ALLOWED_HOSTS = ['smartcrowd.azurewebsites.net','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,10 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
