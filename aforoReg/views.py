a9be74bbb0315fef0dac20635b535946cb029455
[Xime Mzo] <54044787+ximenamzo@users.noreply.github.com>
2023-04-13 14:29:06 -0600

    Probando db
    Abril 13, 2:29pm

--- aforoReg/views.py
+++ aforoReg/views.py
@@ -3,8 +3,10 @@ from django.urls import reverse_lazy
 from . import models
 from django.views import generic
 from django.contrib.messages.views import SuccessMessageMixin
+from .models import Place, Camera, Register
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    lugaresRegistrados = Place.objects.all()
+    return render(request, 'index.html', {'lugares': lugaresRegistrados})
 
